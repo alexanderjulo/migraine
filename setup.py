b6f1c5cd588a5666d8f2b139d96460657cb26c98
Alexander Jung-Loddenkemper <alexander@julo.ch>
2016-09-13 13:28:08 +0200

    bump version to v1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='migraine',
-    version='0.1.0',
+    version='1.0.0',
     description='Datastore independent migration tool',
     author='Alexander Jung-Loddenkemper',
     author_email='alexander@julo.ch',
