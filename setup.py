676531c79747eb2d5f49ecdd0ab2e3276eb4ca38
Alexander Jung-Loddenkemper <alexander@julo.ch>
2016-09-13 13:41:44 +0200

    less specific requirements.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='triptan',
-    version='1.0.0',
+    version='1.0.1',
     description='Datastore independent migration tool',
     author='Alexander Jung-Loddenkemper',
     author_email='alexander@julo.ch',
     url='https://github.com/alexanderjulo/triptan',
     packages=find_packages(),
     install_requires=[
-        'Click==6.0',
-        'click-log==0.1.4',
-        'PyYAML==3.11',
-        'jinja2==2.7.3'
+        'Click==6',
+        'click-log==0',
+        'PyYAML==3',
+        'jinja2==2'
     ],
     tests_require=['pytest'],
     entry_points={
